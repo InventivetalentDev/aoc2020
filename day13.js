c4bfe89ded3a2eb28927a33e575f440e7d62957d
inventivetalent <mail@inventivetalent.org>
2020-12-13 13:03:51 +0100

    day13 2

--- day13.js
+++ day13.js
@@ -29,7 +29,25 @@ const part1 = input => {
 // ======
 
 const part2 = input => {
-    return input
+    const lines = util.getLines(input);
+    const ids = lines[1].split(",").map(i => {
+        if (i === "x") return 1;
+        return parseInt(i);
+    })
+
+    // https://github.com/xiel/advent-of-code/blob/main/src/Day%2013%20-%20Shuttle%20Search/Day13.test.ts#L74
+    let inc = ids[0];
+    let index = 1;
+    let time = inc;
+    while (index < ids.length) {
+        if ((time + index) % ids[index] === 0) {
+            inc *= ids[index];
+            index++;
+        } else {
+            time += inc;
+        }
+    }
+    return time;
 }
 
 module.exports = {part1, part2}
