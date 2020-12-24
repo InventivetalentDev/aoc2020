b19f6a8f8576caf6d4c94594118e647ed3365ac0
inventivetalent <mail@inventivetalent.org>
2020-12-24 16:06:20 +0100

    day24 2

--- day24.js
+++ day24.js
@@ -18,22 +18,51 @@ const dirMap = {
 
 const part1 = input => {
     const directions = util.getLines(input).map(parseLine);
-    // const data = {};
-    // for (let dir of directions) {
-    //     let key = dir.join(",");
-    //     if (!data.hasOwnProperty(key)) {
-    //         data[key] = {
-    //             black: true,
-    //             flips: 1
-    //         };
-    //     } else {
-    //         let x = data[key];
-    //         data[key] = {
-    //             black: !x.black,
-    //             flips: x.flips + 1
-    //         };
-    //     }
-    // }
+    const data = parseInitialState(directions);
+    console.log(JSON.stringify(data, null, 2));
+    return countBlack(data);
+}
+
+// Part 2
+// not working :shrug: https://github.com/AidarT/aoc2020/blob/main/aoc2020day24.js
+// ======
+
+const part2 = input => {
+    const directions = util.getLines(input).map(parseLine);
+    let data = parseInitialState(directions);
+    console.log(JSON.stringify(data, null, 2));
+    for (let i = 0; i <= 100; i++) {
+        let dataCopy = JSON.parse(JSON.stringify(data));
+        for (let x in data) {
+            for (let y in data[x]) {
+                for (let z in data[x][y]) {
+                    let obj = JSON.parse(JSON.stringify(get3(data, +x, +y, +z) || {}));
+                    console.log(obj)
+                    let adjacentBlack = countAdjacentBlack(data, +x, +y, +z);
+                    console.log(adjacentBlack)
+                    if (obj && obj.black === true) {
+                        if (adjacentBlack === 0 || adjacentBlack > 2) {
+                            obj.black = false;
+                            obj.flips++;
+                        }
+                    } else {
+                        if (adjacentBlack === 2) {
+                            obj.black = true;
+                            obj.flips++;
+                        }
+                    }
+                    console.log(obj)
+                    set3(dataCopy, +x, +y, +z, obj);
+                }
+            }
+        }
+        data = JSON.parse(JSON.stringify(dataCopy));
+        console.log(i + " " + countBlack(data));
+    }
+    return countBlack(data);
+}
+
+function parseInitialState(directions) {
     const data = {};
     for (let dir of directions) {
         let pos = [0, 0, 0];
@@ -43,24 +72,38 @@ const part1 = input => {
             pos[1] += ch[1];
             pos[2] += ch[2];
         }
-        let x = get3(data, pos[0], pos[1], pos[2]);
-        if (!x) {
-            x = {
+        let d = get3(data, pos[0], pos[1], pos[2]);
+        if (!d) {
+            d = {
                 black: true,
                 flips: 1
             };
         } else {
-            x.black = !x.black;
-            x.flips++;
+            d.black = !d.black;
+            d.flips++;
         }
-        set3(data, pos[0], pos[1], pos[2], x);
+        set3(data, pos[0], pos[1], pos[2], d);
     }
-    console.log(JSON.stringify(data, null, 2));
+    return data;
+}
+
+function countAdjacentBlack(data, x, y, z) {
+    let c = 0;
+    for (let d of Object.values(dirMap)) {
+        let g = get3(data, x + d[0], y + d[1], z + d[2]);
+        if (g && g.black) {
+            c++;
+        }
+    }
+    return c;
+}
+
+function countBlack(data) {
     let blackTiles = 0;
     for (let x in data) {
         for (let y in data[x]) {
             for (let z in data[x][y]) {
-                let obj = data[x][y][z];
+                let obj = get3(data, +x, +y, +z);
                 if (obj) {
                     if (obj.black) blackTiles++;
                 }
@@ -68,13 +111,6 @@ const part1 = input => {
         }
     }
     return blackTiles;
-}
-
-// Part 2
-// ======
-
-const part2 = input => {
-    return input
 }
 
 function set3(data, x, y, z, v) {
